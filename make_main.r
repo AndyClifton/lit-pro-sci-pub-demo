9e824fcdf2caef5089e4fa37c5e8d5b5610ec0c5
Andy C <andy_clifton@yahoo.com>
2019-10-18 22:49:52 +0200

    BETA: keep intermediate md

--- make_main.r
+++ make_main.r
@@ -7,5 +7,4 @@ require(knitr)
 # Let's generate all of the possible outputs from main
 
 rmarkdown::render("main.rmd", output_format=c('pdf_document2'))
-rmarkdown::render("main.rmd", output_format=c('md_document'))
 rmarkdown::render("main.rmd", output_format=c('html_document'))
