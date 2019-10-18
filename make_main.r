624c2dcf89241a77e9d4e528f2bb6628d7630125
Andy C <andy_clifton@yahoo.com>
2019-10-18 21:28:08 +0200

    BETA: Good enough for a rider on a galloping horse

--- make_main.r
+++ make_main.r
@@ -2,6 +2,12 @@
 
 # Let's generate all of the possible outputs from main
 
-rmarkdown::render("main.rmd", output_format=c('md_document'))
-rmarkdown::render("main.rmd", output_format=c('html_document'))
-rmarkdown::render("main.rmd", output_format=c('pdf_document2'))
+rmarkdown::render("main.rmd",
+  output_format=c('md_document'),
+  output_dir = c('publications'))
+rmarkdown::render("main.rmd",
+  output_format=c('html_document'),
+  output_dir = c('publications'))
+rmarkdown::render("main.rmd",
+  output_format=c('pdf_document2'),
+  output_dir = c('publications'))
